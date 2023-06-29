039bf292276618c815c72e2a582f8c61b8100ea3
Мурат Ханов <Gadgi9512@gmail.com>
2023-06-29 13:00:56 +0300

    Code is done

---
+++ Task-8.6.3/Task-8.6.3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит число. Число сохраняется в HashSet коллекцию.
+// Если такое число уже присутствует в коллекции, то пользователю на экран выводится сообщение, что число уже вводилось ранее.
+// Если числа нет, то появляется сообщение о том, что число успешно сохранено. 
+
+HashSet<int> ints = new HashSet<int>();
+
+Console.WriteLine("Вводите числа, каждое с новой строки для сохранения");
+
+void AddNewNumsInInts()
+{
+    int.TryParse(Console.ReadLine(), out int num);
+
+    if(ints.Contains(num))
+    {
+        Console.WriteLine("Такое число уже присутствует в коллекции");
+        return;
+    }
+    else 
+    {
+        ints.Add(num); 
+        Console.WriteLine("Число записано"); 
+    }
+}
+
+while (true)
+{
+    AddNewNumsInInts();
+}
+
